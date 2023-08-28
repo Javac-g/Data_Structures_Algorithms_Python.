8f1955f9030835dab8d0f46caee1e759c4440fe9
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-08-28 14:47:58 +0300

    Update apples.py

--- IO/apples.py
+++ IO/apples.py
@@ -1,3 +1,5 @@
+#n schoolchildren divide k apples equally, the non-divisible remainder remains in the basket. How many apples will each student get? 
+#How many apples will be left in the basket? The program receives the numbers n and k as input and must output the desired number of apples (two numbers).
 import math
 n = int(input('students'))
 k = int(input('apple'))
